873755483ada249476649f537ef5fbce6925fdc1
Gavra Buckman <gavypython@gmail.com>
2021-12-14 14:54:40 -0600

    Updated requirements.txt to match installed packages

--- main.py
+++ main.py
@@ -23,7 +23,10 @@ Bootstrap(app)
 gravatar = Gravatar(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
+uri = os.environ.get("DATABASE_URL")
+if uri and uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
